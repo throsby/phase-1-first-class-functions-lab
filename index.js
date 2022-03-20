210eea0a79eae2812d59f8a0b43a6281de1c5395
Throsby Wells <throsbywells@gmail.com>
2022-03-20 13:19:20 -0400

    commit tree c8f7437c7cbc4ba6074ec3f54eb6ddede0be75cf

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(arr) {
+    return arr.slice(0,2)
+}
+
+function returnLastTwoDrivers(arr) {
+    return arr.slice(arr.length - 2, arr.length)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier (mult) {
+    return function(fare) {return mult*fare}
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(arr, func) {
+    if (func === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(arr)
+    }
+    else if (func === returnLastTwoDrivers) {
+        return returnLastTwoDrivers(arr)
+    }
+    return 0
+}
